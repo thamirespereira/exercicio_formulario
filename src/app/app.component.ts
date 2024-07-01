73b8e2c0e83e0176de0a4642fdb68824c19069a4
thamirespereira <thamiresfrpereira@hotmail.com>
2024-07-01 19:09:25 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormularioComponent } from "./components/formulario/formulario.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, FormularioComponent]
 })
 export class AppComponent {
   title = 'exercicio_formulario';
---
+++ src/app/components/formulario/formulario.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormField, MatInput, MatInputModule } from '@angular/material/input';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatToolbar } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-formulario',
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatInput,
+    MatToolbar,
+    MatFormField,
+    MatButton,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  templateUrl: './formulario.component.html',
+  styleUrl: './formulario.component.css'
+})
+export class FormularioComponent {
+colunasDaTabela = ['nome', 'telefone', 'email', 'dataDeNascimento'];
+dadosDaTabela = [
+  {nome: '', telefone: '', email: '', dataDeNascimento: ''}
+];
+conteudoDaTabela = new MatTableDataSource(this.dadosDaTabela);
+
+formularioCadastro = new FormGroup(
+  {
+    nome: new FormControl('', Validators.required),
+    telefone: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    dataDeNascimento: new FormControl('', Validators.required)
+  }
+);
+
+novoFormulario: any[] = [];
+
+enviarFormulario(pessoa : any){
+  this.novoFormulario = this.conteudoDaTabela.data.slice();
+  this.novoFormulario.push({nome: pessoa.nome, telefone: pessoa.telefone, email: pessoa.email, dataDeNascimento: pessoa.dataDeNascimento});
+  this.conteudoDaTabela.data = this.novoFormulario;
+  alert("Cadastrado!");
+}
+}
